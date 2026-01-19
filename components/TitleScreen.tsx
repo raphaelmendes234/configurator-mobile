7c8f30fd433d322494d531d704dfa167784643f3
Raphaël <raphamendes@yahoo.com>
2026-01-19 15:11:19 +0100

    feat: title screen & animation

--- components/TitleScreen.tsx
+++ components/TitleScreen.tsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { useEffect } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withRepeat,
+    withTiming,
+} from "react-native-reanimated";
 
-export function TitleScreen(){
-    return (
-        <Text style={{color: "white"}}>Title Screen</Text>
-    )
+export function TitleScreen() {
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+
+  const imgOpacity = useSharedValue(1);
+  const imgScale = useSharedValue(1);
+
+  useEffect(() => {
+    imgOpacity.value = withRepeat(withTiming(0, { duration: 2000 }), -1, true);
+    imgScale.value = withRepeat(withTiming(0.5, { duration: 2000 }), -1, true);
+  }, []);
+
+  const imgStyle = useAnimatedStyle(() => ({
+    opacity: imgOpacity.value,
+    transform: [{ scale: imgScale.value }],
+  }));
+
+  return (
+    <Animated.Image
+      source={require("@/assets/images/icon.png")}
+      style={[imgStyle, styles.image]}
+    ></Animated.Image>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
